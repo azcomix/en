cb8239dc095335678e8b9b3cf1aabc3871571ec4
azcomix <64530544+azcomix@users.noreply.github.com>
2020-05-03 13:01:33 +0200

    Create sevenmoons.js

---
+++ showfromurl/sevenmoons.js
@@ -0,0 +1,12 @@
+//sevenmoons.js
+
+var url=[
+"http://1.bp.blogspot.com/-jDDSPIDP9Pw/U3o0Gty1aYI/AAAAAAAAMfA/WAKdryBF2Jo/s1600/epicillustrated02-42.jpg",
+"http://3.bp.blogspot.com/--nrRnVyxTUo/U3o0GfOYIzI/AAAAAAAAMe0/Cd5N1ka6LCg/s1600/epicillustrated02-43.jpg",
+"http://2.bp.blogspot.com/-KTfDzJxNKHk/U3o0GoutIsI/AAAAAAAAMe4/0Yb4JsjLo5w/s1600/epicillustrated02-44.jpg",
+"http://1.bp.blogspot.com/-hv3s8RRABfk/U3o0IgILwnI/AAAAAAAAMfM/GZFxUun-Nvo/s1600/epicillustrated02-45.jpg",
+"http://1.bp.blogspot.com/-7FO6XCX52ck/U3o0KmEvAaI/AAAAAAAAMfU/OmEGkj8IFLI/s1600/epicillustrated02-46.jpg",
+"http://1.bp.blogspot.com/-q8EQqzoiPCQ/U3o0Ky78fNI/AAAAAAAAMfY/cbmnes3eAFs/s1600/epicillustrated02-47.jpg",
+"http://2.bp.blogspot.com/-XM9T_kihjp4/U3o0LiPnHbI/AAAAAAAAMfc/JeNp4XfQyQQ/s1600/epicillustrated02-48.jpg",
+"http://1.bp.blogspot.com/-hD19PYFNVno/U3o0M4o5e3I/AAAAAAAAMfs/3mYUVK2OrJI/s1600/epicillustrated02-49.jpg"];
+
